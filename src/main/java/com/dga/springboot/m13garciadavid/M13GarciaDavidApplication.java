4ea2bafbe0abde4de473fd8c99d68263fdd90a78
David García Alonso <a20davgaralo@inspedralbes.cat>
2022-03-30 11:55:40 +0200

    Primer commit

---
+++ src/main/java/com/dga/springboot/m13garciadavid/M13GarciaDavidApplication.java
@@ -0,0 +1,13 @@
+package com.dga.springboot.m13garciadavid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M13GarciaDavidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M13GarciaDavidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dga/springboot/m13garciadavid/M13GarciaDavidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dga.springboot.m13garciadavid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M13GarciaDavidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
