83ed7b34e330a71aea68bf309b5a594b5c998345
David García Alonso <a20davgaralo@inspedralbes.cat>
2022-04-01 23:25:55 +0200

    Funcional validació telèfon

--- src/main/java/com/dga/springboot/m13garciadavid/validation/TelefonRegex.java
+++ src/main/java/com/dga/springboot/m13garciadavid/validation/TelefonRegex.java
@@ -1,7 +1,17 @@
 package com.dga.springboot.m13garciadavid.validation;
 
+import javax.validation.Constraint;
 import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Constraint(validatedBy = TelefonRegexValidador.class)
+@Retention(RUNTIME)
+@Target({FIELD, METHOD})
 public @interface TelefonRegex {
     String message() default "Telèfon incorrecte, ha de contenir nou números i començar per 6, 7 o 9";
 
