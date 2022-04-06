9c408ff3b8346412bd23792016acd2fbe3acc502
David Garcia <a20davgaralo@inspedralbes.cat>
2022-04-06 10:20:50 +0200

    Valida CIF i NIF

--- src/main/java/com/dga/springboot/m13garciadavid/validation/IDFiscalRegexValidador.java
+++ src/main/java/com/dga/springboot/m13garciadavid/validation/IDFiscalRegexValidador.java
@@ -1,12 +1,20 @@
 package com.dga.springboot.m13garciadavid.validation;
 
+import com.dga.springboot.m13garciadavid.models.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class IDFiscalRegexValidador implements ConstraintValidator<IDFiscalRegex, String> {
+
+    private final String REGEX_CIF = "([ABCDEFGHJKLMNPQRSUVW])(\\d{7})([0-9A-J])";
+    private final String REGEX_DNI = "[0-9]{8}[A-Z&&[^IOUÑ]]";
+
+
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        if (s.matches("[0-9]{8}[A-Z&&[^IOUÑ]]")) {
+        if (s.matches(REGEX_DNI) || s.matches(REGEX_CIF) ) {
             return true;
         }
         return false;
