c6d94d6119f634eb7e1d4c494eedb519db9e5b22
David Garcia <a20davgaralo@inspedralbes.cat>
2022-04-08 14:22:15 +0200

    Continua desenvolupament millores a vista factura en PDF. Potser acabat. Fer merge

--- src/main/java/com/dga/springboot/m13garciadavid/view/pdf/FacturaPdfView.java
+++ src/main/java/com/dga/springboot/m13garciadavid/view/pdf/FacturaPdfView.java
@@ -29,10 +29,13 @@ import java.util.Map;
 @Component("factura/ver")
 public class FacturaPdfView extends AbstractPdfView {
 
-    private static Font tituloF = new Font(Font.BOLD, 18, Font.TIMES_ROMAN);
-    private static Font peque = new Font(Font.ITALIC, 10, Font.TIMES_ROMAN);
-    private static Font normal = new Font(Font.TIMES_ROMAN, 12);
-    private static Font tituloP = new Font(Font.BOLD, 13, Font.TIMES_ROMAN);
+    private static final Font TITULO_F = new Font(Font.BOLD, 18, Font.TIMES_ROMAN);
+    private static final Font PEQUE_F = new Font(Font.ITALIC, 10, Font.TIMES_ROMAN);
+    private static final Font NORMAL_F = new Font(Font.TIMES_ROMAN, 12);
+    private static final Font TITULO_PEQ_F = new Font(Font.BOLD, 13, Font.TIMES_ROMAN);
+
+    public static final float TITULO_CELL_PADDING = 8f;
+    public static final float CELL_PADDING = 5f;
 
     @Override
     protected void buildPdfDocument(Map<String, Object> model, Document document,
@@ -54,15 +57,15 @@ public class FacturaPdfView extends AbstractPdfView {
         document.add(logo);
 
         //Títol
-        Paragraph titulo = new Paragraph("FACTURA", tituloF);
+        Paragraph titulo = new Paragraph("FACTURA", TITULO_F);
         titulo.setAlignment(Element.ALIGN_CENTER);
         titulo.setSpacingAfter(20);
         document.add(titulo);
 
         Paragraph datosFis = new Paragraph();
-        datosFis.add(new Paragraph("NIF - 49098111C", peque));
-        datosFis.add(new Paragraph("C/ Guillem Tell, 24", peque));
-        datosFis.add(new Paragraph("08006 - Barcelona", peque));
+        datosFis.add(new Paragraph("NIF - 49098111C", PEQUE_F));
+        datosFis.add(new Paragraph("C/ Guillem Tell, 24", PEQUE_F));
+        datosFis.add(new Paragraph("08006 - Barcelona", PEQUE_F));
         datosFis.setAlignment(Element.ALIGN_RIGHT);
         datosFis.setIndentationRight(20);
         document.add(datosFis);
@@ -75,73 +78,130 @@ public class FacturaPdfView extends AbstractPdfView {
         //Per poder modificar els atributs de les cel·les, creem objectes de la classe PdfPCell
         PdfPCell cell = null;
 
-        cell = new PdfPCell(new Phrase("Datos del cliente"));
+        cell = new PdfPCell(new Phrase("Datos del cliente", TITULO_PEQ_F));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         cell.setBackgroundColor(new Color(173, 208, 255));
-        cell.setPadding(8f);
+        cell.setPadding(TITULO_CELL_PADDING);
         tabla.addCell(cell);
 
-        tabla.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
-        tabla.addCell(factura.getCliente().getIdentificacionFiscal());
-        tabla.addCell(factura.getCliente().getEmail());
+        cell = new PdfPCell(new Phrase(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido()));
+        cell.setPadding(CELL_PADDING);
+        tabla.addCell(cell);
+
+        cell = new PdfPCell(new Phrase(factura.getCliente().getIdentificacionFiscal()));
+        cell.setPadding(CELL_PADDING);
+        tabla.addCell(cell);
+
+        cell = new PdfPCell(new Phrase(factura.getCliente().getEmail()));
+        cell.setPadding(CELL_PADDING);
+        tabla.addCell(cell);
 
         PdfPTable tabla2 = new PdfPTable(1);
         tabla2.setSpacingAfter(20);
 
-        cell = new PdfPCell(new Phrase("Datos de la factura"));
+        cell = new PdfPCell(new Phrase("Datos de la factura", TITULO_PEQ_F));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         cell.setBackgroundColor(new Color(191, 255, 173));
-        cell.setPadding(8f);
+        cell.setPadding(TITULO_CELL_PADDING);
         tabla2.addCell(cell);
 
-        tabla2.addCell("Número factura: " + factura.getId());
-        tabla2.addCell("Descripción: " + factura.getDescripcion());
-        tabla2.addCell("Fecha: " + factura.getCreateAt());
+        cell = new PdfPCell(new Phrase("Número factura: " + factura.getId()));
+        cell.setPadding(CELL_PADDING);
+        tabla2.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Descripción: " + factura.getDescripcion()));
+        cell.setPadding(CELL_PADDING);
+        tabla2.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Fecha: " + factura.getCreateAt()));
+        cell.setPadding(CELL_PADDING);
+        tabla2.addCell(cell);
 
         //Guardem les taules creades a través de Document
         document.add(tabla);
         document.add(tabla2);
 
         PdfPTable tabla3 = new PdfPTable(4);
+        cell = new PdfPCell(new Phrase("Detalle de la factura", TITULO_PEQ_F));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setBackgroundColor(new Color(255, 173, 185));
+        cell.setPadding(TITULO_CELL_PADDING);
+        cell.setColspan(4); //Fa que la cel·la ocupi quatre columnes
+        tabla3.addCell(cell);
+
         tabla3.setWidths(new float[] {3.5f, 1, 1, 1});
-        tabla3.addCell(new Paragraph("Producto", tituloP));
-        tabla3.addCell(new Paragraph("Precio", tituloP));
-        tabla3.addCell(new Paragraph("Cantidad", tituloP));
-        tabla3.addCell(new Paragraph("Total", tituloP));
+
+        cell = new PdfPCell(new Phrase("Producto", TITULO_PEQ_F));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setPadding(CELL_PADDING);
+        tabla3.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Precio", TITULO_PEQ_F));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setPadding(CELL_PADDING);
+        tabla3.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Cantidad", TITULO_PEQ_F));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setPadding(CELL_PADDING);
+        tabla3.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Total", TITULO_PEQ_F));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setPadding(CELL_PADDING);
+        tabla3.addCell(cell);
 
         //Afegir els elements de la factura
         for(ItemFactura item: factura.getItems()) {
-            tabla3.addCell(item.getProducto().getNombre());
-            tabla3.addCell(item.getProducto().getPrecio().toString());
+
+            cell = new PdfPCell(new Phrase(item.getProducto().getNombre()));
+            cell.setPadding(CELL_PADDING);
+            tabla3.addCell(cell);
+
+            cell = new PdfPCell(new Phrase(item.getProducto().getPrecio().toString()));
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell.setPadding(CELL_PADDING);
+            tabla3.addCell(cell);
 
             cell = new PdfPCell(new Phrase(item.getCantidad().toString()));
             cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+            cell.setPadding(CELL_PADDING);
             tabla3.addCell(cell);
-            tabla3.addCell(item.calcularImporte().toString());
+
+            cell = new PdfPCell(new Phrase(item.calcularImporte().toString()));
+            cell.setPadding(CELL_PADDING);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            tabla3.addCell(cell);
         }
 
         //Footer. Creem una cel·la per ella
-        cell = new PdfPCell(new Phrase("Total"));
+        cell = new PdfPCell(new Phrase("Total", TITULO_PEQ_F));
         //Numero columnas
         cell.setColspan(3);
+        cell.setPadding(CELL_PADDING);
         //Alinear text a la dreta
         cell.setHorizontalAlignment(PdfCell.ALIGN_RIGHT);
-
         //Afegir a la tabla3
         tabla3.addCell(cell);
-        tabla3.addCell(factura.getTotal().toString().concat("€"));
+
+        cell = new PdfPCell(new Phrase(factura.getTotal().toString().concat("€"), TITULO_PEQ_F));
+        cell.setPadding(CELL_PADDING);
+        cell.setHorizontalAlignment(PdfCell.ALIGN_RIGHT);
+        tabla3.addCell(cell);
 
         document.add(tabla3);
 
         Paragraph pago = new Paragraph();
         pago.setSpacingBefore(40);
-        pago.add(new Paragraph("Puede hacer efectivo el ingreso del importe por transferencia al número de cuenta siguiente:", normal));
-        pago.add(new Paragraph("IBAN: ES49 XXXX XXXX XXXX XXXX", normal));
+        pago.add(new Paragraph("Puede hacer efectivo el ingreso del importe por transferencia al número de cuenta siguiente:", NORMAL_F));
+        pago.add(new Paragraph("IBAN: ES49 XXXX XXXX XXXX XXXX", NORMAL_F));
         pago.setAlignment(Element.ALIGN_LEFT);
         pago.setIndentationLeft(40);
         document.add(pago);
 
         Paragraph firma = new Paragraph();
         firma.setSpacingBefore(40);
-        firma.add(new Paragraph("Firmado", peque));
+        firma.add(new Paragraph("Firmado", PEQUE_F));
         firma.setAlignment(Element.ALIGN_RIGHT);
         firma.setIndentationRight(40);
         document.add(firma);
